1548d99507f5ddae8a21c264f1708534a428ae81
carlos-vanoni <carlosedu.vanoni@gmail.com>
2023-06-22 17:19:12 -0300

    commit

---
+++ PG-TB/Hello3D - Pyramid/Object.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <vector>
+#include <stb_image.h>
+
+#include "Mesh.h"
+
+using namespace std;
+
+class Object
+{
+public:
+	Object() {}
+	void initialize(string filePath, string texturePath, Shader* shader, glm::vec3 position, glm::vec3 scale, float angle = 0.0, glm::vec3 axis = glm::vec3(0.0, 0.0, 1.0));
+	void update();
+	void draw();
+protected:
+	void loadObj(string filePath, string texturePath);
+	GLuint generateVAO(vector<GLfloat> vertbuffer, int& nVerts);
+	GLuint loadTexture(string filePath);
+	vector <Mesh> grupos;
+
+	//Informações sobre as transformações a serem aplicadas no objeto
+	glm::vec3 position;
+	glm::vec3 scale;
+	float angle;
+	glm::vec3 axis;
+
+	Shader* shader;
+};
---
+++ README.md
@@ -0,0 +1,2 @@
+# computacao-grafica-unisinos-tb-2023-1
+Trabalho do Grau B - Unisinos - Computação Gráfica - 2023/1
